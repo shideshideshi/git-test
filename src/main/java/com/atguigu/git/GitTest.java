2c69241f9694ab556e2753ea6aa7b53a8cb75f5c
wwb <1280370411@qq.com>
2022-03-29 18:44:59 +0800

    hot  commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git3");
+        System.out.println("hello,git3");
     }
 }
